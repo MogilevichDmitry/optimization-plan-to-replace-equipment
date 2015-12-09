0a8cee0af9d04210d387b4b0b2db43b2102fc6a5
MogilevichDmitry <dmsmain@gmail.com>
2015-12-10 00:49:29 +0300

    v0.4

--- optimization/optim.cs
+++ optimization/optim.cs
@@ -41,25 +41,27 @@ namespace optimization
             int ss;
             double w;
             double d;
+            bool logic = false;
             Random rand = new Random();     // генерируем число от 0 до 1
             double alpha;                   // по равномерному закону распределения
             List<double> V_alpha = new List<double>();
+            Console.WriteLine("Введите кол-во типов различных устройств: ");
             M = Int32.Parse(Console.ReadLine());
-
-            /* п.1 */
             for (int i = 0; i < M; i++)// число устройств k-го типа
             {
+                Console.WriteLine("Введите число устройств k-го типа: ");
                 int temp = Int32.Parse(Console.ReadLine());
                 Mi.Add(temp);
                 SumMachines += temp;
-
             }
             for (int i = 0; i < SumMachines; i++) // add L_k //add MY_ik // add Aikn
             {
+                Console.WriteLine("Введите нормативный срок эксплуатации устройства k-го типа: ");
                 int tempL_k = Int32.Parse(Console.ReadLine());
                 L_k.Add(tempL_k);
                 for (int j = 0; j < Mi[i]; j++)
                 {
+                    Console.WriteLine("Введите время эксплуатации i-го устройства k-го типа: ");
                     int tempMY_ik = Int32.Parse(Console.ReadLine());
                     MY_ik.Add(tempMY_ik);
                     int tempAik = 0;
@@ -70,16 +72,18 @@ namespace optimization
                     A_ikn.Add(tempAik);
                 }
             }
+            /* п.1 */
             double tempVn;
-            for (int i = 0; i < N; i++)
+            for (int k = 0; k < M; k++)
             {
                 tempVn = 0;
-                for (int j = 0; j < M; j++) // add C_k // Vn 
+                for (int i = 0; i < Mi[k]; i++) // add C_k // Vn 
                 {
+                    Console.WriteLine("Введите трудоемкость обслуживания одной единицы k-го типа: ");
                     double tempC_k = Double.Parse(Console.ReadLine());
                     C_k.Add(tempC_k);
                     double tempSumAi = 0;
-                    for (int q = 0; q < Mi[j]; q++)
+                    for (int q = 0; q < Mi[k]; q++)
                     {
                         tempSumAi += A_ikn[q];
                     }
@@ -88,7 +92,7 @@ namespace optimization
                 Vn.Add(tempVn);
             }
             tempVn = 0;
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < Vn.Count; i++) 
             {
                 tempVn += Vn[i];
             }
@@ -97,9 +101,9 @@ namespace optimization
             {
                 B_ikn.Add(A_ikn[i]);
             }
-            for (int p = 0; p < N; p++)
+            for (int p = 0; p < A_ikn.Count; p++)
             {
-                if (p == n) //????
+                if (p == n) 
                 {
                     X_iknp.Add(A_ikn[p]);
                 }
@@ -108,24 +112,34 @@ namespace optimization
                     X_iknp.Add(0);
                 }
             }
-            for (int i = 0; i < N - 1; i++)
+            for (int i = 0; i < N; i++)
             {
+                Console.WriteLine("Введите коэффициент корректировки трудоёмкости: ");
                 double tempR_n = Double.Parse(Console.ReadLine());
                 R_n.Add(tempR_n);
             }
             for ( int i = 0; i < M; i++ )
             {
+                Console.WriteLine("Введите предельный допуск на уменьшение срока замены k-го типа: ");
                 double tempt1_k = Double.Parse(Console.ReadLine());
                 t_1k.Add(tempt1_k);
+                Console.WriteLine("Введите предельный допуск на увеличение срока замены k-го типа: ");
                 double tempt2_k = Double.Parse(Console.ReadLine());
                 t_2k.Add(tempt2_k);
             }
             /* п.2 */
             for (int k = 0; k < M; k++) 
             {
-                /* п.3 */
+                /* п.3 */               
                 for (n = 0; n < N - 1; n++)
                 {
+                p3:
+                    if (logic == true)
+                    {
+                        n++;
+                        logic = false;
+                    }
+
                     /* п.4 */
                     s = (int)(V_sr * R_n[n] / C_k[k]);
                     ss = (int)((V_sr * R_n[n]) % C_k[k]);
@@ -158,17 +172,63 @@ namespace optimization
                     }
                     if (w > 0)
                     {
-                        //перейти к п.5 
+                        //перейти к п.5+++
+                        goto p5;
                     }
                     else if (w <= 0)
                     {
-                        //перейти к п.8
+                        //перейти к п.8+++
+                        for (int i = 0; i < Mi[k]; i++)
+                        {
+                            /* п. 9 */
+                            for (int j = 1; j < F_min(t_2k[k], N, n); j++)
+                            {
+                                /* п.10 */
+                                if (B_ikn[n] == 0)
+                                {
+                                    //перейти к п.8+++
+                                    break;                                      
+                                }
+                                else
+                                {
+                                    while (X_iknp[n + j - 1] == 0)
+                                    {
+                                        //перейти к п.9+++
+                                        break;
+                                    }
+                                    d = w + C_k[k];
+                                }
+                                for (int lambda = n + 1; lambda < N; lambda += L_k[k])
+                                {
+                                    X_iknp[lambda + j] = 1;
+                                    B_ikn[lambda] = 1;
+                                    V_alpha[lambda] = V_alpha[lambda] + C_k[k];
+                                }
+                                for (int lambda = n; lambda < N; lambda += L_k[k])
+                                {
+                                    X_iknp[lambda - 1] = 0;
+                                    B_ikn[lambda] = 0;
+                                    V_alpha[lambda] = V_alpha[lambda] - C_k[k];
+                                }
+                                if (d < C_k[k])
+                                {
+                                    //перейти к п.3
+                                    logic = true;
+                                    goto p3;
+                                }
+                                else
+                                {
+                                    w = d;
+                                }
+                            }
+                        }
                     }
                     /* п.5  */
+                    p5:
                     for ( int j = 0; j < F_min(t_1k[k],N, n); j++)
                     {
                         /* п.6 */
-                        for (int i = 0; i < Mi[k]; i++) //????
+                        for (int i = 0; i < Mi[k]; i++) 
                         {
                             /* п.7 */
                             while (B_ikn[n] == 1 || B_ikn[n + j] == 0) 
@@ -176,7 +236,7 @@ namespace optimization
                                 for (int lambda = n; lambda < N; lambda += L_k[k]) 
                                 {
                                     B_ikn[lambda] = 1;
-                                    V_alpha[lambda] = V_alpha[lambda] + C_k[k]; // ????
+                                    V_alpha[lambda] = V_alpha[lambda] + C_k[k]; 
                                 }
                                 for (int lambda = n + j; lambda < N; lambda += L_k[k])
                                 {
@@ -188,7 +248,9 @@ namespace optimization
                                 d = w - C_k[k];
                                 if (d < C_k[k])
                                 {
-                                    //перейти к п.3 
+                                    //перейти к п.3                                    
+                                    logic = true;
+                                    goto p3;
                                 }
                                 else
                                 {
@@ -196,22 +258,24 @@ namespace optimization
                                 }
                                 break; //перейти к п.6 +++
                             }
-                            /* п. 8 */
+                            /* п. 8 */                           
                             for (i = 0; i < Mi[k]; i++)
                             {
                                 /* п. 9 */
                                 for (j = 1; j < F_min(t_2k[k], N, n); j++)
                                 {
+                                    /* п.10 */
                                     if (B_ikn[n] == 0)
                                     {
-                                        //перейти к п.8
+                                        //перейти к п.8 +++
+                                        break;                                     
                                     }
                                     else
                                     {
                                         while (X_iknp[n + j - 1] == 0)
                                         {
-                                            //перейти к п.9 
-                                            break;//????????????
+                                            //перейти к п.9++ 
+                                            break;
                                         }
                                         d = w + C_k[k];
                                     }
@@ -227,14 +291,16 @@ namespace optimization
                                         B_ikn[lambda] = 0;
                                         V_alpha[lambda] = V_alpha[lambda] - C_k[k];
                                     }
-                                    if (d < C_k[k]) // ????
-                                    { 
+                                    if (d < C_k[k])
+                                    {
                                         //перейти к п.3
+                                        logic = true;
+                                        goto p3;
                                     }
                                     else
                                     {
                                         w = d;
-                                    }                                    
+                                    }                                 
                                 }
                             }
                         }
